ad2af8d36d134583dabe7527849591a178de7cb7
Vishal Kumar <ketuvishal@gmail.com>
2020-01-22 22:16:08 +0530

    addition to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Counter = ({id, value}) => { 
+const Counter = ({id, value, onDelete, onIncrement, counter}) => { 
     return (
         <div>
             <div>Counter #{id}</div>
             <span className="badge badge-warning m-2">{value}</span>
-            <button className="btn btn-primary btn-sm m-2">Increment</button>
-            <button className="btn btn-danger btn-sm">Delete</button>
+            <button onClick={()=>onIncrement(counter)} className="btn btn-primary btn-sm m-2">Increment</button>
+            <button onClick={()=>onDelete(id)} className="btn btn-danger btn-sm">Delete</button>
         </div>
         );
 }
--- src/components/Counters.js
+++ src/components/Counters.js
@@ -11,16 +11,32 @@ class Counters extends Component {
         ]
     }
 
-    handleIncrement =(id)=>{
-        const counters = [...this.state.counters];
-        counters
+    handleDelete =(idValue) => {
+        const counters = this.state.counters.filter(data =>data.id!==idValue)
+        this.setState({counters});
     }
+    handleIncrement = counter =>{
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] ={...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+
     render() { 
         return (
             <React.Fragment>
+                <nav class="navbar navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                </nav>
                 {
                     this.state.counters.map(data=>{
-                        return <Counter key={data.id} id={data.id} value={data.value}/>
+                        return <Counter 
+                        onDelete={this.handleDelete}
+                        key={data.id} id={data.id}
+                        value={data.value}
+                        onIncrement={this.handleIncrement}
+                        counter={data}/>
                     })
                 }
             </React.Fragment>
